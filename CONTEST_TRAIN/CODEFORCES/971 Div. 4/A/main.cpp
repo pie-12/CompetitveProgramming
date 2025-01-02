401037b6a7790a26bf989a3124c94d5f653ee4e7
pie-12 <tunglamnguyenxd@gmail.com>
2025-01-02 21:39:40 +0700

    test vim

--- CONTEST_TRAIN/CODEFORCES/971 Div. 4/A/main.cpp
+++ CONTEST_TRAIN/CODEFORCES/971 Div. 4/A/main.cpp
@@ -1,37 +1,19 @@
-//yo, i'm pie12 (・ω・)ﾉ 
+//pie-12 (◕‿◕) 
 #include <bits/stdc++.h>
-#define HI  signed main()
-#define ll long long
-#define ld long double
-#define endl '\n'
 using namespace std;
-#ifndef ONLINE_JUDGE
-#define IO \
-freopen("main.inp","r",stdin); \
-freopen("main.out","w",stdout);
-#else
-#define IO
-#endif
-#define FAST \
-ios::sync_with_stdio(false); \
-cin.tie(nullptr);
-#define TIME (1.0 * clock() / CLOCKS_PER_SEC)
-#define BYE \
-cerr <<"Time elapsed: " << TIME << " s\n"; \
-return 0;
-
-HI {
-    IO FAST
-	
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 	int t; cin >> t;
-	while(t--){
+	while(t--)
+	{
 		int a, b; cin >> a >> b;
-		int res = 314159;
+		int ans = INT_MAX;
 		for(int i = a; i <= b; i++){
-			int temp = (i - a) + (b - i);
-			res = min(res, temp);
+			ans = min(ans, i - a + b - i);
 		}
-		cout << res << endl;
-	}    
-    BYE
+		cout << ans << endl;
+	}
+    return 0;
 }
