46af45c09811dc522101dd22d2217d8510d6b847
pie-12 <tunglamnguyenxd@gmail.com>
2025-01-12 23:39:09 +0700

    div 2, tut rating toi choi

--- CONTEST_TRAIN/CF OTHER/HELLO 2025/A/main.cpp
+++ CONTEST_TRAIN/CF OTHER/HELLO 2025/A/main.cpp
@@ -12,5 +12,11 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     
+    int t; cin >> t;
+    while(t--)
+    {
+        int a, b; cin >> a >> b;
+        cout << max(a,b) + 1 << '\n';
+    }
     return 0;
 }
