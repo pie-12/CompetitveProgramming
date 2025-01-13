8bb474e24f2cd2a6a9070ad365a0e67f1bfc8683
pie-12 <tunglamnguyenxd@gmail.com>
2025-01-13 22:53:07 +0700

    div2 996 prob C

--- CONTEST_TRAIN/CF DIV/996 Div. 2/C/main.cpp
+++ CONTEST_TRAIN/CF DIV/996 Div. 2/C/main.cpp
@@ -1,71 +1,20 @@
+//pie-12 (◕‿◕) 
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
 
-int main() {
+void solve()
+{
+    
+}
+
+int main()
+{
     #ifndef ONLINE_JUDGE
-        freopen("main.inp", "r", stdin);
-        freopen("main.out", "w", stdout);
+    freopen("main.inp","r",stdin);
+    freopen("main.out","w",stdout);
     #endif
-
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int n, m;
-    string path;
-    cin >> n >> m >> path;
-
-    vector<vector<int>> grid(n, vector<int>(m, 0));
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cin >> grid[i][j];
-        }
-    }
-
-    // Xác định đường đi
-    vector<pair<int, int>> on_path;
-    int x = 0, y = 0;
-    on_path.push_back({x, y});
-    for (char step : path) {
-        if (step == 'D') x++;
-        else if (step == 'R') y++;
-        on_path.push_back({x, y});
-    }
-
-    // Tính tổng hàng và cột
-    vector<int> row_sum(n, 0), col_sum(m, 0);
-    vector<vector<bool>> is_path(n, vector<bool>(m, false));
-    for (auto [i, j] : on_path) {
-        is_path[i][j] = true;
-    }
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (!is_path[i][j]) {
-                row_sum[i] += grid[i][j];
-                col_sum[j] += grid[i][j];
-            }
-        }
-    }
-
-    // Tìm tổng ma trận (magic_sum)
-    int total_sum = accumulate(row_sum.begin(), row_sum.end(), 0);
-    int magic_sum = total_sum / n;
-
-    // Điền giá trị cho các ô trên đường đi
-    int value = 1;
-    for (auto [i, j] : on_path) {
-        grid[i][j] = magic_sum - row_sum[i] - col_sum[j] + grid[i][j];
-        row_sum[i] += grid[i][j];
-        col_sum[j] += grid[i][j];
-    }
-
-    // Xuất kết quả
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cout << grid[i][j] << " ";
-        }
-        cout << "\n";
-    }
-
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    int t=1; while(t--)solve();
     return 0;
 }
--- START/Form.cpp
+++ START/Form.cpp
@@ -1,47 +1,20 @@
-//pie12 です
+//pie-12 (◕‿◕) 
 #include <bits/stdc++.h>
-using namespace std;
-using ii = pair<int, int>;
-using iii = pair<int, ii>;
 #define ll long long
-#define ull unsigned long long
-#define ld long double
-#define endl '\n'
-#define PI 3.1415926535897932384626433832795
-#define all(a) (a).begin(),(a).end()
-#define rall(a) (a).rbegin(),(a).rend()
-#define pb push_back
-#define fi first
-#define se second
-#define fast ios::sync_with_stdio(false); cin.tie(nullptr);
-#define time cerr << "Time run: " << (1000 * clock() / CLOCKS_PER_SEC) << " ms\n";
-#define FOR(i, a, b) for (int i = a; i < b; i++)
-#define F0R(i, a) for(int i = 0; i < a; i++)
-#define Set(A, n) memset(A, n, sizeof(A))
+using namespace std;
 
-string to_upper(string s) {for(int i=0;i<(int)s.size();++i)if(s[i]>='a' && s[i]<='z')s[i]-='a'-'A';return s;}
-string to_lower(string s) {for(int i=0;i<(int)s.size();++i)if(s[i]>='A' && s[i]<='Z')s[i]+='a'-'A';return s;}
-ll gcd(ll a,ll b) {if (b==0) return a; return gcd(b, a%b);}
-ll lcm(ll a,ll b) {return a/gcd(a,b)*b;}
-ll nCk (int n, int k){if(k== 0||n==k)return 1;return nCk(n-1,k)+nCk(n-1,k-1);}
-ll factorial(ll n){if(n<=1)return 1;return n*factorial(n-1);}
-bool isprime(ll a) {if(a==1)return 0; for(int i=2;i<=round(sqrt(a));++i) if(a%i==0)return 0; return 1;}
-void yes() {cout<<"YES\n";}
-void no() {cout<<"NO\n";}
-
-const int MAX_N = 1e5 + 5;
-const ll MOD = 1e9 + 7;
-const ll INF = 1e9;
+void solve()
+{
+    
+}
 
 int main()
-{  
+{
     #ifndef ONLINE_JUDGE
-        freopen("main.inp","r",stdin);
-        freopen("main.out","w",stdout);
+    freopen("main.inp","r",stdin);
+    freopen("main.out","w",stdout);
     #endif
-    fast
-
-    
-    time
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    int t; cin >> t; while(t--) solve();
     return 0;
 }
